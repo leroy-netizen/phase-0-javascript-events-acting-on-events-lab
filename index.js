28a3452fe03d5286592a00a9a09cea34881a0df0
Leroy-netizen <leroythomas051@gmail.com>
2022-06-29 16:39:51 +0300

    commit tree 2bfb8aa623f74aeede3cffc3e3c8943792d511bd

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const dodger = document.querySelector("#dodger");
 const moveDodgerLeft = () => (dodger.style.left = "50px");
 const moveDodgerRight = () => (dodger.style.left = "250px");
+Object.keys(employee);
